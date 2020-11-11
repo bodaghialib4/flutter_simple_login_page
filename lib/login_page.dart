7e8b9ab5c7972fdb53543afb411781243e8134a2
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-11 12:27:27 +0330

    adding logo widget into LoginPage widget (in login_page.dart)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,6 +8,17 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    final logo = Padding(
+      padding: EdgeInsets.all(20),
+      child: Hero(
+        tag: 'hero',
+        child: SizedBox(
+          height: 160,
+          child: Image.asset('assets/logo.png'),
+        ),
+      ),
+    );
+
     // TODO: implement build
     throw UnimplementedError();
   }
