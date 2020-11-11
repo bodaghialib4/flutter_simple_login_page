2d25ece700beeac654bada0ca8109351335adec3
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-11 12:58:16 +0330

    adding avatar widget into Dashboard widget (in dashboard.dart)

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final avatar = Padding(
+      padding: EdgeInsets.all(20),
+      child: Hero(
+        tag: 'login',
+        child: SizedBox(
+          height: 160,
+          child: Image.asset('assets/ali.png'),
+        ),
+      ),
+    );
+
     // TODO: implement build
     throw UnimplementedError();
   }
