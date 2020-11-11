fdafb14bf4b142ffbee66cf3b1ffa0ed0276db67
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-11 12:54:50 +0330

    create dashboard.dart with a stateless widget inside it

---
+++ lib/dashboard.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
